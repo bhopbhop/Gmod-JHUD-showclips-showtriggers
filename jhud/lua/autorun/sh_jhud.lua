8795e8e21990faf8ba754caac8ed7d8b47a324c1
bhopbhop <87729531+bhopbhop@users.noreply.github.com>
2022-05-29 17:01:05 -0400

    Delete jhud/lua directory

--- jhud/lua/autorun/sh_jhud.lua
+++
@@ -1,8 +0,0 @@
-if SERVER then
-	AddCSLuaFile("jhud/cl_jhud.lua")
-	AddCSLuaFile("jhud/cl_menu.lua")
-	include("jhud/sv_jhud.lua")
-else
-	include("jhud/cl_jhud.lua")
-	include("jhud/cl_menu.lua")
-end
